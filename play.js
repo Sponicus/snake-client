ca8bd3ad61c650232143bc9b3a7d2f1cce26c212
Andrew <andrew.spon@gmail.com>
2021-11-03 18:56:35 +0000

    refactor play.js && input && client

--- play.js
+++ play.js
@@ -1,37 +1,10 @@
 //requires function to connect from client.js
+//requires functions to read input from input.js
 const {connect} = require('./client');
-
-
+const {setupInput} = require('./input');
 
 console.log("Connecting...");
 connect();
-////////////////CREATE hnadleUserInput FUNCTION//////////////////////
-const handleUserInput = (key) => {
-  if (key === 'w') {
-    process.stdin.write('Move: up')
-  }
-  if (key === 'a') {
-    process.stdin.write('Move: left')
-  }
-  if (key === 's') {
-    process.stdin.write('Move: down')
-  }
-  if (key === 'd') {
-    process.stdin.write('Move: right')
-  }
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-//////////////////////////////////////////
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);///added to call on handleUserInput function
-  return stdin;
-};
 
 setupInput();
+
